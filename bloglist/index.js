c549d4166c76977aa3797c947d8d94123af2a164
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2024-06-22 21:39:10 +0700

    finished exercise 4.1 - 4.2

---
+++ bloglist/index.js
@@ -0,0 +1,17 @@
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const Blog = require('./models/blog')
+
+app.use(cors())
+app.use(express.json())
+
+const PORT = 3003
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+})
