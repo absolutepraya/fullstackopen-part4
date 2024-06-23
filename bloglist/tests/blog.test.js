00bf91f6d974ab77f6b2fb472eae206b0fe6cd28
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2024-06-23 22:21:24 +0700

    finished exercise 4.6 - 4.7

--- bloglist/tests/blog.test.js
+++ bloglist/tests/blog.test.js
@@ -11,61 +11,8 @@ describe('dummy test', () => {
     })
 })
 
-// for totalLikes test and favoriteBlog test
-const oneBlog = [
-    {
-        _id: '5a422aa71b54a676234d17f8',
-        title: 'Go To Statement Considered Harmful',
-        author: 'Edsger W. Dijkstra',
-        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-        likes: 5,
-        __v: 0
-    }
-]
-
-// for totalLikes test and favoriteBlog test
-const blogs = [
-    {
-        _id: '5a422aa71b54a676234d17f8',
-        title: 'Go To Statement Considered Harmful',
-        author: 'Edsger W. Dijkstra',
-        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-        likes: 5,
-        __v: 0
-    },
-    {
-        _id: '5a422b3a1b54a676234d17f9',
-        title: 'Introduction to Algorithms',
-        author: 'Thomas H. Cormen',
-        url: 'https://mitpress.mit.edu/books/introduction-algorithms-third-edition',
-        likes: 10,
-        __v: 0
-    },
-    {
-        _id: '5a422b891b54a676234d17fa',
-        title: 'The Art of Computer Programming',
-        author: 'Donald E. Knuth',
-        url: 'http://www-cs-faculty.stanford.edu/~knuth/taocp.html',
-        likes: 12,
-        __v: 0
-    },
-    {
-        _id: '5a422ba71b54a676234d17fb',
-        title: 'Clean Code: A Handbook of Agile Software Craftsmanship',
-        author: 'Robert C. Martin',
-        url: 'https://www.pearson.com/us/higher-education/program/Martin-Clean-Code-A-Handbook-of-Agile-Software-Craftsmanship/PGM319733.html',
-        likes: 8,
-        __v: 0
-    },
-    {
-        _id: '5a422bc61b54a676234d17fc',
-        title: 'You Don’t Know JS',
-        author: 'Kyle Simpson',
-        url: 'https://github.com/getify/You-Dont-Know-JS',
-        likes: 15,
-        __v: 0
-    }
-]
+// import dummy blogs
+const { oneBlog, blogs } = require('./test_blogs')
 
 describe('total likes', () => {
 
@@ -76,7 +23,7 @@ describe('total likes', () => {
 
     test('sum of likes from multiple blogs', () => {
         const result = listHelper.totalLikes(blogs)
-        assert.strictEqual(result, 50)
+        assert.strictEqual(result, 36)
     })
 })
 
@@ -88,6 +35,20 @@ describe('favorite blog', () => {
 
     test('favorite blog from multiple blogs', () => {
         const result = listHelper.favoriteBlog(blogs)
-        assert.deepStrictEqual(result, blogs[4])
+        assert.deepStrictEqual(result, blogs[2])
+    })
+})
+
+describe('most blogs', () => {
+    test('author with most blogs', () => {
+        const result = listHelper.mostBlogs(blogs)
+        assert.deepStrictEqual(result, { author: 'Robert C. Martin', blogs: 3 })
+    })
+})
+
+describe('most likes', () => {
+    test('author with most likes', () => {
+        const result = listHelper.mostLikes(blogs)
+        assert.deepStrictEqual(result, { author: 'Edsger W. Dijkstra', likes: 17 })
     })
 })
--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+const lodash = require('lodash')
+
 const dummy = (blogs) => {
     return 1
 }
@@ -10,8 +12,25 @@ const favoriteBlog = (blogs) => {
     return blogs.reduce((max, blog) => blog.likes > max.likes ? blog : max, blogs[0])
 }
 
+const mostBlogs = (blogs) => {
+    const countByAuthor = lodash.countBy(blogs, 'author');
+    const authorCounts = lodash.toPairs(countByAuthor);
+    const maxAuthor = lodash.maxBy(authorCounts, 1);
+    return { author: maxAuthor[0], blogs: maxAuthor[1] };
+}
+
+const mostLikes = (blogs) => {
+    const groupedByAuthor = lodash.groupBy(blogs, 'author');
+    const likesByAuthor = lodash.mapValues(groupedByAuthor, (blogs) => lodash.sumBy(blogs, 'likes'));
+    const authorLikes = lodash.toPairs(likesByAuthor);
+    const maxAuthor = lodash.maxBy(authorLikes, 1);
+    return { author: maxAuthor[0], likes: maxAuthor[1] };
+}
+
 module.exports = {
-    dummy, 
+    dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
