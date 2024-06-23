12c9beb6f95c42c1c83a26ce03be0f3d00822047
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2024-06-23 21:17:43 +0700

    finished exercise 4.3

--- bloglist/index.js
+++ bloglist/index.js
@@ -13,5 +13,5 @@ app.use(express.json())
 
 const PORT = 3003
 app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`)
+    console.log(`server running on port ${PORT}, url: http://localhost:${PORT}`)
 })
