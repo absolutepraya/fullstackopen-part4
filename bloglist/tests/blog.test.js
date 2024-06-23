785c32ed8c238b4284a151e6077fc57d6a47db1b
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2024-06-23 22:06:14 +0700

    finished exercise 4.5

--- bloglist/tests/blog.test.js
+++ bloglist/tests/blog.test.js
@@ -11,60 +11,63 @@ describe('dummy test', () => {
     })
 })
 
-describe('total likes', () => {
-    const oneBlog = [
-        {
-            _id: '5a422aa71b54a676234d17f8',
-            title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
-            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-            likes: 5,
-            __v: 0
-        }
-    ]
+// for totalLikes test and favoriteBlog test
+const oneBlog = [
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 5,
+        __v: 0
+    }
+]
 
-    const blogs = [
-        {
-            _id: '5a422aa71b54a676234d17f8',
-            title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
-            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-            likes: 5,
-            __v: 0
-        },
-        {
-            _id: '5a422b3a1b54a676234d17f9',
-            title: 'Introduction to Algorithms',
-            author: 'Thomas H. Cormen',
-            url: 'https://mitpress.mit.edu/books/introduction-algorithms-third-edition',
-            likes: 10,
-            __v: 0
-        },
-        {
-            _id: '5a422b891b54a676234d17fa',
-            title: 'The Art of Computer Programming',
-            author: 'Donald E. Knuth',
-            url: 'http://www-cs-faculty.stanford.edu/~knuth/taocp.html',
-            likes: 12,
-            __v: 0
-        },
-        {
-            _id: '5a422ba71b54a676234d17fb',
-            title: 'Clean Code: A Handbook of Agile Software Craftsmanship',
-            author: 'Robert C. Martin',
-            url: 'https://www.pearson.com/us/higher-education/program/Martin-Clean-Code-A-Handbook-of-Agile-Software-Craftsmanship/PGM319733.html',
-            likes: 8,
-            __v: 0
-        },
-        {
-            _id: '5a422bc61b54a676234d17fc',
-            title: 'You Don’t Know JS',
-            author: 'Kyle Simpson',
-            url: 'https://github.com/getify/You-Dont-Know-JS',
-            likes: 15,
-            __v: 0
-        }
-    ]
+// for totalLikes test and favoriteBlog test
+const blogs = [
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 5,
+        __v: 0
+    },
+    {
+        _id: '5a422b3a1b54a676234d17f9',
+        title: 'Introduction to Algorithms',
+        author: 'Thomas H. Cormen',
+        url: 'https://mitpress.mit.edu/books/introduction-algorithms-third-edition',
+        likes: 10,
+        __v: 0
+    },
+    {
+        _id: '5a422b891b54a676234d17fa',
+        title: 'The Art of Computer Programming',
+        author: 'Donald E. Knuth',
+        url: 'http://www-cs-faculty.stanford.edu/~knuth/taocp.html',
+        likes: 12,
+        __v: 0
+    },
+    {
+        _id: '5a422ba71b54a676234d17fb',
+        title: 'Clean Code: A Handbook of Agile Software Craftsmanship',
+        author: 'Robert C. Martin',
+        url: 'https://www.pearson.com/us/higher-education/program/Martin-Clean-Code-A-Handbook-of-Agile-Software-Craftsmanship/PGM319733.html',
+        likes: 8,
+        __v: 0
+    },
+    {
+        _id: '5a422bc61b54a676234d17fc',
+        title: 'You Don’t Know JS',
+        author: 'Kyle Simpson',
+        url: 'https://github.com/getify/You-Dont-Know-JS',
+        likes: 15,
+        __v: 0
+    }
+]
+
+describe('total likes', () => {
 
     test('sum of likes from one blog', () => {
         const result = listHelper.totalLikes(oneBlog)
@@ -74,5 +77,17 @@ describe('total likes', () => {
     test('sum of likes from multiple blogs', () => {
         const result = listHelper.totalLikes(blogs)
         assert.strictEqual(result, 50)
+    })
+})
+
+describe('favorite blog', () => {
+    test('favorite blog from one blog', () => {
+        const result = listHelper.favoriteBlog(oneBlog)
+        assert.deepStrictEqual(result, oneBlog[0])
+    })
+
+    test('favorite blog from multiple blogs', () => {
+        const result = listHelper.favoriteBlog(blogs)
+        assert.deepStrictEqual(result, blogs[4])
     })
 })
